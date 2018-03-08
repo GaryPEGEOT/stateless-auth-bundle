402c195f8f4efe4379d0f49aad8142abab589ca1
Gary PEGEOT <g.pegeot@highco-data.fr>
2018-03-08 17:20:52 +0100

    Test token encoder

---
+++ tests/GhostAgency/Bundle/StatelessAuthBundle/Tests/Encoder/TokenEncoderTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of the stateless-auth-bundle package.
+ *
+ * (c) Gary PEGEOT <garypegeot@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GhostAgency\Bundle\StatelessAuthBundle\Tests\Encoder;
+
+use GhostAgency\Bundle\StatelessAuthBundle\Encoder\TokenEncoder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TokenEncoderTest
+ *
+ * @author Gary PEGEOT <garypegeot@gmail.com>
+ */
+class TokenEncoderTest extends TestCase
+{
+    /**
+     * @var TokenEncoder
+     */
+    private $encoder;
+
+    /**
+     * Test JWT encoding.
+     */
+    public function testEncode()
+    {
+        $now = new \DateTimeImmutable('1901-01-01');
+        $payload = [
+            'name' => 'John DOE',
+            'iat'  => $now->getTimestamp(),
+            'exp'  => $now->modify('+1 year'),
+        ];
+        $token = explode('.', $this->encoder->encode($payload));
+
+        $this->assertCount(3, $token);
+        $this->assertEquals('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9', $token[0], 'Header should match');
+        $this->assertEquals(
+            'eyJpYXQiOi0yMTc3NDUzMzYxLCJleHAiOnsiZGF0ZSI6IjE5MDItMDEtMDEgMDA6MDA6MDAuMDAwMDAwIiwidGltZXpvbmVfdHlwZSI6MywidGltZXpvbmUiOiJFdXJvcGVcL1BhcmlzIn0sIm5hbWUiOiJKb2huIERPRSJ9',
+            $token[1],
+            'Payload should match'
+        );
+        $this->assertEquals('6mhEy7FCMNR_4Mbche_qVzmOWiY324EN_eShROMKcLM', $token[2], 'Signature should match');
+    }
+
+    /**
+     * Test JWT decoding.
+     */
+    public function testDecode()
+    {
+        $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o";
+
+        $payload = $this->encoder->decode($token);
+
+        $this->assertEquals('John Doe', $payload->name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->encoder = new TokenEncoder('secret');
+    }
+}
